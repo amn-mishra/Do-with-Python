c7f8363c423497a2208bab289d4176ccc20709d0
Aman Mishra <118250003+amanmishra131@users.noreply.github.com>
2022-11-18 13:19:55 +0530

    Update Send Mail through Python.py

--- Send Mail through Python.py
+++ Send Mail through Python.py
@@ -3,8 +3,8 @@ port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email =   "________@gmail.com"  # Enter your address
 receiver_email = "________@gmail.com"  # Enter receiver address
-password = 'luibrojetjuskbog'
-message = """i am going to send the mail to given email id  """
+password = 'luibrojetjuskbog' # for this password go to your google account setting and then click on security you'll see option app password ,generate password from there
+message = """i am going to send the mail to given email id  """ #whatsoever you want to send write here 
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:

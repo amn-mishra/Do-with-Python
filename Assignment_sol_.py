4f6071397fcc5a3ec2061037c98d59dacd7c523e
Aman Mishra <118250003+amanmishra131@users.noreply.github.com>
2023-02-18 22:38:42 +0530

    Add files via upload

---
+++ Assignment_sol_.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, countDistinct, sum, isnan
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("CSV Statistics").getOrCreate()
+
+def process_csv_stats(csv_url):
+  # Read CSV file
+  df = spark.read.format("csv").option("header", "true").load(csv_url)
+
+  # Calculate total number of rows
+  total_rows = df.count()
+
+  # Calculate total number of columns
+  total_cols = len(df.columns)
+
+  # Calculate number of distinct values and empty/null values for each column
+  col_stats = []
+  for col_name in df.columns:
+    distinct_count = df.select(countDistinct(col(col_name))).collect()[0][0]
+    empty_count = df.filter((col(col_name).isNull()) | (col(col_name) == "")).count()
+    col_stats.append((col_name, distinct_count, empty_count))
+
+  # Save statistics to SQL database
+  # Please replace <your_sql_uri> with your actual SQL database URI
+  df_stats = spark.createDataFrame(col_stats, ["column_name", "distinct_values", "empty_values"])
+  df_stats.write.format("jdbc").option("url", "<your_sql_uri>").option("dbtable", "csv_stats").mode("overwrite").save()
+
+def get_csv_stats(csv_url):
+  # Retrieve statistics from SQL database
+  # Please replace <your_sql_uri> with your actual SQL database URI
+  df_stats = spark.read.format("jdbc").option("url", "<your_sql_uri>").option("dbtable", "csv_stats").load()
+
+  # Filter statistics for the given CSV file
+  csv_stats = df_stats.filter(col("column_name") == csv_url)
+
+  # Return statistics as a dictionary
+  return csv_stats.select("distinct_values", "empty_values").rdd.map(lambda x: x.asDict()).collect()[0]
